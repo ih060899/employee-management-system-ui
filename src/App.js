8132ea2f4d424d99952ec55fc4f403761cefef34
Imran Hossain <ih060899@users.noreply.github.com>
2022-03-20 11:19:32 +0600

    add empolyee form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AddEmployee from './components/AddEmployee';
 import Navbar from './components/Navbar';
 
 function App() {
     return (
         <>
             <Navbar />
+            <AddEmployee />
         </>
     );
 }
