061304d5791f64e4f0a349d651439ce06bb25a2d
Imran Hossain <ih060899@users.noreply.github.com>
2022-03-20 13:26:26 +0600

    sava data to server

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import EmployeeService from '../services/EmployeeService';
 
 const AddEmployee = () => {
-    const { employee, setEmployee } = useState({
+    const [employee, setEmployee] = useState({
         id: '',
         firstName: '',
         lastName: '',
@@ -14,7 +15,12 @@ const AddEmployee = () => {
 
     const saveEmployee = e => {
         e.preventDefault();
-    }
+        EmployeeService.saveEmployee(employee)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => console.log(error));
+    };
     return (
         <div className='flex max-w-2xl mx-auto shadow border-b'>
             <div className='px-8 py-8 '>
@@ -60,13 +66,13 @@ const AddEmployee = () => {
                     />
                 </div>
                 <div className='items-center justify-center h-14 w-full my-4 space-x-4 pt-4'>
-                    <button className='rounded text-white font-semibold bg-green-400 px-6 py-2 hover:bg-green-700'>
+                    <button
+                        className='rounded text-white font-semibold bg-green-400 px-6 py-2 hover:bg-green-700'
+                        onClick={saveEmployee}>
                         Save
                     </button>
 
-                    <button
-                        className='rounded text-white font-semibold bg-red-400 px-6 py-2 hover:bg-red-700'
-                        onClick={saveEmployee}>
+                    <button className='rounded text-white font-semibold bg-red-400 px-6 py-2 hover:bg-red-700'>
                         Clear
                     </button>
                 </div>
